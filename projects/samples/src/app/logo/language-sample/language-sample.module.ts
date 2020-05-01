caad3469a2494201eb5238be4e197028e6527c5c
Serkan KONAKCI <konakci@mail.com>
2020-05-01 23:27:55 +0300

    refactor(samples): add language module sample safeHTML pipe

--- projects/samples/src/app/logo/language-sample/language-sample.module.ts
+++ projects/samples/src/app/logo/language-sample/language-sample.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { LanguageInitSetting, LanguageModule } from '@logo-software/language';
 import { LanguageSampleRoutingModule } from './language-sample-routing.module';
 import { LanguageShowcaseComponent } from './language-showcase/language-showcase.component';
+import { SharedPipeModule } from '@logo-software/core';
 
 const languageConf: LanguageInitSetting = {
   abbr: 'ro', readFromFile: false, extension: 'json', path: 'languages',
@@ -14,6 +15,7 @@ const languageConf: LanguageInitSetting = {
     CommonModule,
     LanguageSampleRoutingModule,
     LanguageModule.forRoot(languageConf),
+    SharedPipeModule,
   ],
 })
 export class LanguageSampleModule {
