d1a8d061937a50064441f94212eef38a57fdd920
Serkan KONAKCI <konakci@mail.com>
2020-05-01 23:31:18 +0300

    style(paging): paging routing fix

--- projects/samples/src/app/logo/paging-sample/paging-sample.module.ts
+++ projects/samples/src/app/logo/paging-sample/paging-sample.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagingShowcaseComponent } from './paging-showcase/paging-showcase.component';
 import { PagingModule } from '@logo-software/paging';
+import { PagingSampleRoutingModule } from './paging-sample-routing.module';
 
 @NgModule({
   declarations: [PagingShowcaseComponent],
-  imports: [CommonModule, PagingModule],
+  imports: [CommonModule, PagingModule, PagingSampleRoutingModule],
 })
 export class PagingSampleModule {
 }
