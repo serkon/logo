34080fab169dc056326224bceb48ac4818257bac
Serkan KONAKCI <konakci@mail.com>
2020-06-08 02:19:57 +0300

    style(docs): improvement interface of the developer.logo.com.tr

--- projects/logo-software/icons/generate-icons.js
+++ projects/logo-software/icons/generate-icons.js
@@ -1,18 +1,33 @@
+const {series, task, src, dest, pipe} = require('gulp');
 const fs = require('fs');
 const path = require('path');
 const {writeFileSync} = require('fs-extra');
+const postcss = require('gulp-postcss');
+const processors = [
+  require('postcss-strip-inline-comments'),
+  require('postcss-inline-svg'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')({overrideBrowserslist: ['last 2 versions', '> 2%']}),
+];
+const pscss = require('postcss-scss');
+
 const ICONS_DIRECTORY = './src/lib/assets/icons/';
 const ICONS_TS_OUTPUT = './src/lib/logo-icons.ts';
 const ICONS_SCSS_OUTPUT = './src/lib/logo-icons.scss';
-const list = [];
+const OUT = './src/lib/';
+const ts = [];
 let scss = '';
-fs.readdirSync(ICONS_DIRECTORY).forEach(file => {
-  const name = path.parse(file).name;
-  list.push(name);
-  scss = `${scss}
+
+task('string', (cb) => {
+  fs.readdirSync(ICONS_DIRECTORY).forEach(file => {
+    const name = path.parse(file).name;
+    ts.push(name);
+    scss = `${scss}
 .logo-i-${name} {
   // &::before{
-    background-image: url("/assets/icons/${name}.svg");
+    background-image:  svg-load("assets/icons/${name}.svg", fill=#000000); // url("/assets/icons/${name}.svg");
     background-repeat: no-repeat;
     background-position: center;
     // display: inline-block;
@@ -23,12 +38,34 @@ fs.readdirSync(ICONS_DIRECTORY).forEach(file => {
     // left: 0;
   // }
 }
-  `;
+`;
+  });
+  cb();
 });
 
-const createTSFile = (input, output) => {
-  writeFileSync(output,
-    `
+task('scss', (cb) => {
+  writeFileSync(ICONS_SCSS_OUTPUT, `
+/**
+* @license
+* Copyright LOGO YAZILIM SANAYİ VE TİCARET A.Ş. All Rights Reserved.
+*
+* Save to the extent permitted by law, you may not use, copy, modify,
+* distribute or create derivative works of this material or any part
+* of it without the prior written consent of LOGO YAZILIM SANAYİ VE TİCARET A.Ş. Limited.
+* Any reproduction of this material must contain this notice.
+*/
+
+@import "~@logo-software/theme/src/lib/style";
+
+// IMPORTANT: THIS SCSS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+${scss}
+`, {encoding: 'utf-8'},
+  );
+  cb();
+});
+
+task('typescript', (cb) => {
+  writeFileSync(ICONS_TS_OUTPUT, `
 /**
  * @license
  * Copyright LOGO YAZILIM SANAYİ VE TİCARET A.Ş. All Rights Reserved.
@@ -40,33 +77,21 @@ const createTSFile = (input, output) => {
  */
 
 // IMPORTANT: THIS TS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-export const LOGO_ICONS = ${JSON.stringify(input, null, 2).replace(/\"/g, "'")};
-`,
-    {encoding: 'utf-8'},
-  );
-}
-
-const createSCSSFile = (input, output) => {
-  writeFileSync(output,
-    `/**
- * @license
- * Copyright LOGO YAZILIM SANAYİ VE TİCARET A.Ş. All Rights Reserved.
- *
- * Save to the extent permitted by law, you may not use, copy, modify,
- * distribute or create derivative works of this material or any part
- * of it without the prior written consent of LOGO YAZILIM SANAYİ VE TİCARET A.Ş. Limited.
- * Any reproduction of this material must contain this notice.
- */
-
-@import "~@logo-software/theme/src/lib/style";
-
-// IMPORTANT: THIS SCSS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-  ${input}
-
+export const LOGO_ICONS = ${JSON.stringify(ts, null, 2).replace(/\"/g, "'")};
 `, {encoding: 'utf-8'},
   );
-}
+  cb();
+});
 
-createTSFile(list, ICONS_TS_OUTPUT);
-createSCSSFile(scss, ICONS_SCSS_OUTPUT);
-console.log(`### ${list.length} icons were generated`);
+task('postCSS', (cb) => {
+  return src(ICONS_SCSS_OUTPUT)
+    .pipe(postcss(processors, {syntax: pscss}))
+    .pipe(dest(OUT));
+  console.log('postCSS');
+  cb()
+});
+
+task('generate', series('string', 'scss', 'typescript', 'postCSS', (cb) => {
+  console.log(`### ${ts.length} icons were generated`);
+}));
+task('generate')();
--- projects/samples/src/app/logo/input-sample/input-showcase/input-showcase.component.ts
+++ projects/samples/src/app/logo/input-sample/input-showcase/input-showcase.component.ts
@@ -1,16 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'logo-input-showcase',
   templateUrl: './input-showcase.component.html',
   styleUrls: ['./input-showcase.component.scss'],
 })
-export class InputShowcaseComponent implements OnInit {
+export class InputShowcaseComponent implements OnInit, AfterViewInit {
+  public quotes;
+  public links;
+  private readonly bodyHeight: number;
 
-  constructor() {
+  constructor(@Inject(DOCUMENT) private document) {
+    this.bodyHeight = this.document.body.clientHeight;
+    this.quotes = [
+      {story: 'People ignore design that ignores people', author: 'FRANK CHIMERO'},
+      {story: 'Good designs come from the heart, not from the brain', author: 'DANNY SENGERS'},
+    ];
+    this.links = {
+      one: [],
+      four: [
+        {url: '#', display: 'Corporate'},
+        {url: '#', display: 'Contact'},
+        {url: '#', display: 'Blog'},
+        {url: '#', display: 'Carier'},
+      ],
+      footer: {
+        socials: [{url: '#', display: 'twitter'}, {url: '#', display: 'facebook'}, {url: '#', display: 'youtube'}],
+        terms: [
+          {url: '#', display: 'Copyright'},
+          {url: '#', display: 'Terms of Use'},
+          {url: '#', display: 'Privacy'},
+          {url: '#', display: 'Personal Data'},
+          {url: '#', display: 'Cookie Policy'},
+        ],
+      },
+    };
+  }
+
+  private _sections: HTMLElement[] = [];
+
+  get sections() {
+    return this._sections;
+  }
+
+  set sections(value) {
+    this._sections = value;
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.sections = this.document.querySelectorAll('section');
+    this.sections.forEach(section => {
+      section.style.minHeight = `${this.bodyHeight}px`;
+    });
   }
 
 }
