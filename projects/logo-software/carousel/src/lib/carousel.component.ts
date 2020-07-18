3cb6ad190a0767d4815ad59f44290fc9929c902d
Serkan KONAKCI <konakci@mail.com>
2020-07-18 15:40:56 +0300

    feat(carousel): add dots feature to carouse

--- projects/logo-software/carousel/src/lib/carousel.component.ts
+++ projects/logo-software/carousel/src/lib/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 
 export interface SliderItem {
   icon: string;
@@ -12,8 +12,8 @@ export interface SliderItem {
 @Component({
   selector: 'logo-carousel',
   template: `
-    <div class="slider">
-      <div class="sliding">
+    <div class="slider" #slider>
+      <div class="sliding" #sliding>
         <ng-content></ng-content>
         <!--
         <img class="icon" src="{{item.icon}}" title="{{item.title}}"/>
@@ -23,52 +23,101 @@ export interface SliderItem {
         <a class="go"></a>
         -->
       </div>
-      <div class="demo">
-        <a href="#" class="slider-arrow left" (click)="onClick($event)">
-          <i></i>
-          <svg>
-            <use xlink:href="#circle"/>
-          </svg>
-        </a>
-        <a href="#" class="slider-arrow" (click)="onClick($event)">
-          <i></i>
-          <svg>
-            <use xlink:href="#circle"/>
-          </svg>
-        </a>
-      </div>
-      <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol
-          id="circle"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 44 44"
-          width="44px"
-          height="44px"
-          fill="none"
-          stroke="currentColor"
-        >
-          <circle r="20" cy="22" cx="22" id="test"/>
-        </symbol>
-      </svg>
     </div>
+    <div class="arrow-with-animation">
+      <a href="#" class="slider-arrow left" (click)="moveLeft($event)">
+        <i></i>
+        <svg>
+          <use xlink:href="#circle"/>
+        </svg>
+      </a>
+      <a href="#" class="slider-arrow" (click)="moveRight($event)">
+        <i></i>
+        <svg>
+          <use xlink:href="#circle"/>
+        </svg>
+      </a>
+    </div>
+    <div class="dots" *ngIf="dots">
+      <a class="dot" *ngFor="let item of maxMoveCountArray; let i = index" [ngClass]="{'active':currentItem===i}"
+         (click)="onDotClick(i)"></a>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+      <symbol
+        id="circle"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 44 44"
+        width="44px"
+        height="44px"
+        fill="none"
+        stroke="currentColor"
+      >
+        <circle r="20" cy="22" cx="22" id="test"/>
+      </symbol>
+    </svg>
   `,
   styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent implements OnInit {
-  @Input() items: SliderItem[] = [];
+export class CarouselComponent implements AfterViewInit {
+  /**
+   * Total will be moved item count, default is one
+   */
+  @Input() count = 1;
+  @Input() dots = false;
+  @ViewChild('slider', {read: ElementRef}) slider: ElementRef;
+  @ViewChild('sliding', {read: ElementRef}) sliding: ElementRef;
+  totalItems: number = 0;
+  currentItem: number = 0;
+  showingCount: number = 0;
+  itemBoxWidth: number = 0;
+  sliderWidth: number = 0;
+  slidingWidth: number = 0;
+  maxSize: number = 0;
+  maxMoveCount: number = 0;
+  maxMoveCountArray = [];
 
-  constructor() {
+  constructor(private elementRef: ElementRef, private cd: ChangeDetectorRef) {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.totalItems = this.sliding.nativeElement.childElementCount;
+    this.itemBoxWidth = this.getItemWidth() * this.count;
+    this.slidingWidth = this.sliding.nativeElement.offsetWidth;
+    this.resize();
+    this.cd.detectChanges();
   }
 
-  onClick(event) {
-    event.preventDefault();
-    this.cursorAnimate(event);
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.reset();
+    this.resize();
+  }
+
+  reset() {
+    this.currentItem = 0;
+    this.sliding.nativeElement.style.transform = `translateX(0px)`;
+  }
+
+  resize() {
+    this.showingCount = Math.floor(this.calculateShowingItems());
+    this.sliderWidth = this.slider.nativeElement.offsetWidth;
+    this.maxSize = this.slidingWidth - this.sliderWidth;
+    this.maxMoveCount = Math.ceil(this.maxSize / this.itemBoxWidth);
+    this.maxMoveCountArray = [...Array(this.maxMoveCount + 1).keys()];
+  }
+
+  calculateShowingItems() {
+    return this.slider.nativeElement.offsetWidth / this.itemBoxWidth;
+  }
+
+  getItemWidth(): number {
+    const elementRef = this.sliding.nativeElement.firstElementChild;
+    return elementRef.offsetWidth + parseFloat(getComputedStyle(elementRef).marginLeft) + parseFloat(getComputedStyle(elementRef).marginRight);
+    // return this.sliding.nativeElement.querySelector(`:nth-child(${index})`).offsetWidth;
   }
 
   cursorAnimate(event) {
+    event.preventDefault();
     const arrow = event.target.closest('.slider-arrow');
     if (!arrow.classList.contains('animate')) {
       arrow.classList.add('animate');
@@ -78,4 +127,31 @@ export class CarouselComponent implements OnInit {
     }
   }
 
+  moveRight(event) {
+    this.cursorAnimate(event);
+    if (this.totalItems >= this.currentItem && this.currentItem < this.maxMoveCount) {
+      this.currentItem = this.currentItem + 1;
+      this.move();
+    }
+  }
+
+  moveLeft(event) {
+    this.cursorAnimate(event);
+    if (this.currentItem >= 1) {
+      this.currentItem = this.currentItem - 1;
+      this.move();
+    }
+  }
+
+  move() {
+    const translateXSize = this.itemBoxWidth * this.currentItem;
+    console.log(translateXSize);
+    this.sliding.nativeElement.style.transform = `translateX(${translateXSize > this.maxSize ? -this.maxSize : -translateXSize}px)`;
+  }
+
+  onDotClick(index: number) {
+    console.log(index);
+    this.currentItem = index;
+    this.move();
+  }
 }
