a44a814b754c4d89e7922a0e84d026d3ec7f9039
Serkan KONAKCI <konakci@mail.com>
2020-08-01 17:38:23 +0300

    style(sample): cursor and icon sample update

--- projects/samples/src/app/logo/cursor-sample/cursor-sample-routing.module.ts
+++ projects/samples/src/app/logo/cursor-sample/cursor-sample-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CursorShowcaseComponent } from '@samples/cursor-sample/cursor-showcase/cursor-showcase.component';
 
-
 const routes: Routes = [{
   path: 'cursor-showcase/cursor-showcase.component',
   component: CursorShowcaseComponent,
--- projects/samples/src/app/logo/icons-sample/icons-sample.module.ts
+++ projects/samples/src/app/logo/icons-sample/icons-sample.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { IconsSampleRoutingModule } from './icons-sample-routing.module';
 import { IconsShowcaseComponent } from './icons-showcase.component';
 import { IconsService } from '@logo-software/icons/src/lib/icons.service';
+import { IconsModule } from '@logo-software/icons';
 
 @NgModule({
   declarations: [IconsShowcaseComponent],
-  imports: [CommonModule, IconsSampleRoutingModule],
+  imports: [CommonModule, IconsSampleRoutingModule, IconsModule],
   providers: [IconsService],
   exports: [IconsShowcaseComponent],
 })
