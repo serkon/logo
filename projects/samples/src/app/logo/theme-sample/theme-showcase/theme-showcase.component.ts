0b992336154c3971abdb4ae8dbe3f426f323232d
Serkan KONAKCI <konakci@mail.com>
2020-08-04 11:41:26 +0300

    refactor(samples): theme samples added

--- projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
+++ projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'logo-theme-showcase',
   templateUrl: './theme-showcase.component.html',
   styleUrls: ['./theme-showcase.component.scss'],
 })
-export class ThemeShowcaseComponent implements OnInit {
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
+export class ThemeShowcaseComponent {
+  statuses = ['brand', 'primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'gray'];
+  states = ['hover', 'active', 'focus'];
+  types = ['fill', 'outline', 'ghost'];
+  shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 }
