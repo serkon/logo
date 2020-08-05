13c4855750a3413f1f01aad48b429ec2b5c60842
Serkan KONAKCI <konakci@mail.com>
2020-08-06 00:16:12 +0300

    feat(sample): theme component color display improvement

--- projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
+++ projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'logo-theme-showcase',
   templateUrl: './theme-showcase.component.html',
   styleUrls: ['./theme-showcase.component.scss'],
 })
-export class ThemeShowcaseComponent {
+export class ThemeShowcaseComponent implements AfterViewInit {
   statuses = ['brand', 'primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'gray'];
   states = ['hover', 'active', 'focus'];
   types = ['fill', 'outline', 'ghost'];
   shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+  constructor(private ngZone: NgZone) {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  getColor(item) {
+    console.log(item);
+    return getComputedStyle(document.documentElement).getPropertyValue(item);
+  }
 }
