d31d7fc30e7e23fa755725c7e6bed51764f854c6
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2020-09-06 22:30:38 +0300

    feat(theme): theme library structure fix

--- projects/logo-software/theme/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of theme
- */
-
-export * from './lib/theme.component';
-export * from './lib/theme.module';
---
+++ projects/logo-software/theme/theme.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright LOGO YAZILIM SANAYİ VE TİCARET A.Ş. All Rights Reserved.
+ *
+ * Save to the extent permitted by law, you may not use, copy, modify,
+ * distribute or create derivative works of this material or any part
+ * of it without the prior written consent of LOGO YAZILIM SANAYİ VE TİCARET A.Ş. Limited.
+ * Any reproduction of this material must contain this notice.
+ */
+
+// IMPORTANT: THIS TS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+export const LOGO_ICONS = {
+  statuses: ['brand', 'primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'gray'],
+  states: ['hover', 'active', 'focus'],
+  types: ['fill', 'outline', 'ghost'],
+  sizes: ['large', 'medium', 'small'],
+  shades: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+};
