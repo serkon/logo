adea0b876980feb5b9a5e97fe50fe1e7ae01301f
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2020-09-08 20:18:09 +0300

    style(sample): add basic color to showcase

--- projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
+++ projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
@@ -6,7 +6,7 @@ import { AfterViewInit, Component, NgZone } from '@angular/core';
   styleUrls: ['./theme-showcase.component.scss'],
 })
 export class ThemeShowcaseComponent implements AfterViewInit {
-  statuses = ['brand', 'primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'gray'];
+  statuses = ['brand', 'primary', 'secondary', 'basic', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'gray'];
   states = ['hover', 'active', 'focus'];
   types = ['fill', 'outline', 'ghost'];
   shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
