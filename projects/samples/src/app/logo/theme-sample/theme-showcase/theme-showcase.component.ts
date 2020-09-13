4590c9b017876c980e6df203ff2f8009e3193414
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2020-09-13 23:17:30 +0300

    refactor(theme): regeneration

--- projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
+++ projects/samples/src/app/logo/theme-sample/theme-showcase/theme-showcase.component.ts
@@ -1,4 +1,9 @@
-import { AfterViewInit, Component, NgZone } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+export enum ELEMENT {
+  BUTTON = 'BUTTON',
+  INPUT = 'INPUT',
+}
 
 @Component({
   selector: 'logo-theme-showcase',
@@ -6,18 +11,41 @@ import { AfterViewInit, Component, NgZone } from '@angular/core';
   styleUrls: ['./theme-showcase.component.scss'],
 })
 export class ThemeShowcaseComponent implements AfterViewInit {
-  statuses = ['brand', 'primary', 'secondary', 'basic', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'gray'];
+  statuses = ['brand', 'primary', 'secondary', 'basic', 'light', 'dark', 'gray'];
+  semantic = ['success', 'info', 'warning', 'danger'];
   states = ['hover', 'active', 'focus'];
   types = ['fill', 'outline', 'ghost'];
   shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+  elements = {
+    button: {
+      name: ELEMENT.BUTTON,
+      render: {
+        fill: true,
+        outline: true, // ['primary', 'secondary', 'light', 'dark', 'gray'],
+        ghost: true,
+      },
+    },
+    input: {
+      name: ELEMENT.INPUT,
+      render: {
+        fill: true,
+        outline: true, // ['primary', 'secondary', 'light', 'dark', 'gray'],
+        ghost: true,
+      },
+    },
+  };
+  keys: string[] = [];
+  elementType = ELEMENT;
 
-  constructor(private ngZone: NgZone) {
+  constructor() {
+    this.keys = Object.keys(this.elements);
   }
 
   ngAfterViewInit() {
   }
 
   getColor(item) {
+    console.log(item);
     return getComputedStyle(document.documentElement).getPropertyValue(item);
   }
 }
