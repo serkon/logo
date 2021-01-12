00089a7b969d4f89b0f51ef631e5cbac83aed949
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-01-12 12:03:39 +0300

    refactor(guideline): remove unnecessary files

--- projects/guideline/src/app/app.module.ts
+++ projects/guideline/src/app/app.module.ts
@@ -13,18 +13,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {RemoveModule} from './remove/remove.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        RemoveModule
-    ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
