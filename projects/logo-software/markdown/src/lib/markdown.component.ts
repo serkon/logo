e07cc047318bf8bb4a93a067edd4c977ce12c8f1
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-01-18 15:43:26 +0300

    refactor(markdown): read md files and show content using Dynamic Component

--- projects/logo-software/markdown/src/lib/markdown.component.ts
+++ projects/logo-software/markdown/src/lib/markdown.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges, ViewChild, ViewContainerRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as marked from 'marked';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { DynamicService } from '@logo-software/dynamic';
 
 @Component({
   selector: 'logo-markdown',
-  template: `
-    <p>
-      markdown works! yea
-    </p>
-  `,
-  styles: [],
+  styleUrls: ['./markdown.component.scss'],
+  templateUrl: './markdown.component.html',
 })
-export class MarkdownComponent implements OnInit {
+export class MarkdownComponent implements OnChanges, AfterViewInit {
 
-  constructor() {
+  @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
+  @Input() url = '/assets/docs/abc.md';
+  public string: SafeHtml;
+
+  constructor(private dynamicService: DynamicService, private http: HttpClient, private sanitize: DomSanitizer) {
   }
 
-  ngOnInit(): void {
+  async ngOnChanges(changes: SimpleChanges) {
+    console.log(this.url);
   }
+
+  async ngAfterViewInit() {
+    this.string = await this.resolve();
+    this.container.createComponent(this.dynamicService.factory(this.string));
+  }
+
+  async resolve() {
+    return this.convertToHtml(await this.getFile());
+  }
+
+  convertToHtml(content): string {
+    const md = marked.setOptions();
+    return md.parse(content);
+  }
+
+  getFile(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.url, {responseType: 'text'}).subscribe((v) => {
+        resolve(v);
+      });
+    })
+  }
+
 }
--- projects/logo-software/markdown/src/lib/markdown.module.ts
+++ projects/logo-software/markdown/src/lib/markdown.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
+import { CompilerFactory, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { PlaygroundModule } from '@logo-software/playground';
+import { DynamicModule } from '@logo-software/dynamic';
+
 import { MarkdownComponent } from './markdown.component';
+
+export function createCompiler(compilerFactory: CompilerFactory) {
+  return compilerFactory.createCompiler();
+}
 
 @NgModule({
   declarations: [MarkdownComponent],
-  imports: [],
+  imports: [HttpClientModule, PlaygroundModule, DynamicModule],
   exports: [MarkdownComponent],
 })
 export class MarkdownModule {
