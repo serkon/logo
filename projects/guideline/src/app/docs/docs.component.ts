bb04386d3f4d87939de19654d3859575f810ffbb
serhan.gurbuz <serhan.gurbuz@logo.com.tr>
2021-01-21 20:59:32 +0300

    refactor(guideline): improve and development of docs and details pages

--- projects/guideline/src/app/docs/docs.component.ts
+++ projects/guideline/src/app/docs/docs.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { HeaderService } from '../components/header/header.service';
-
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'logo-docs',
   templateUrl: './docs.component.html',
   styleUrls: ['./docs.component.scss'],
 })
 export class DocsComponent implements OnInit {
 
-  constructor(private headerService: HeaderService) {
-    this.headerService.setHeaderTitle('Docs');
+  constructor() {
   }
 
   ngOnInit(): void {
