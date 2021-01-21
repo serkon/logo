88df0639dc85b07577a7926ed25e38e24102ab3d
serhan.gurbuz <serhan.gurbuz@logo.com.tr>
2021-01-21 21:02:09 +0300

    refactor(drawer): change header position and menu opener update

--- projects/logo-software/drawer/src/lib/drawer.service.ts
+++ projects/logo-software/drawer/src/lib/drawer.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DrawerService {
   menu: boolean = true;
+  opener: boolean = true;
+  toggle = new Subject();
 }
