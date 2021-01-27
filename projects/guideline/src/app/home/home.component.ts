00a3ac2d793a1fc1f35152344d5bed746871aa9c
serhan.gurbuz <serhan.gurbuz@logo.com.tr>
2021-01-27 14:28:56 +0300

    feat(guideline): Implementation of Landing page design and mobile support

--- projects/guideline/src/app/home/home.component.ts
+++ projects/guideline/src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DrawerService } from '../../../../logo-software/drawer/src/lib/drawer.service';
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'logo-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
