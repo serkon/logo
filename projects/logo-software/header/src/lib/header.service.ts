147788cdf8631165b4916a0bc271cbb1d856bf77
serhan.gurbuz <serhan.gurbuz@logo.com.tr>
2021-01-27 14:25:54 +0300

    feat(header): Update header library for automatic mobile support and scroll spy customization

--- projects/logo-software/header/src/lib/header.service.ts
+++ projects/logo-software/header/src/lib/header.service.ts
@@ -4,6 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class HeaderService {
+  public mobileMenu: boolean = false;
+  public startTheme: string = 'default';
+  public scrollTheme: string = 'scrolled';
+  public catchPoint: number = 100;
+  public isScrollSpy: boolean = true;
 
   constructor() {
   }
