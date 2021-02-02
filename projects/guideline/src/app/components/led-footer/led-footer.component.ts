492c9d9d229fe2b4c70f5e3d5d802bf8859c6695
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-02-02 18:37:50 +0300

    refactor(guideline): implement new footer design

--- projects/guideline/src/app/components/led-footer/led-footer.component.ts
+++ projects/guideline/src/app/components/led-footer/led-footer.component.ts
@@ -9,6 +9,16 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
+import { ToastService } from '@logo-software/toast';
+
+export interface Subscriber {
+  $key: string;
+  email: string;
+  registration: Date;
+}
 
 @Component({
   selector: 'logo-led-footer',
@@ -16,11 +26,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./led-footer.component.scss'],
 })
 export class LedFooterComponent implements OnInit {
+  public subscribeForm: FormGroup;
+  public subscriber: Subscriber;
+  private subscriberRef: AngularFireList<any>;
 
-  constructor() {
+  constructor(public fb: FormBuilder, private db: AngularFireDatabase, public toast: ToastService) {
   }
 
   ngOnInit(): void {
+    this.subscribeForm = new FormGroup(
+      {
+        'email': new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]),
+      },
+    );
   }
 
+  submitSubscriber() {
+    this.subscriber = this.subscribeForm.value;
+    this.subscriberRef = this.db.list('subscribers');
+    this.subscriberRef.push({
+      email: this.subscriber.email,
+      registration: new Date().toISOString(),
+    }).catch(e => {
+      this.showToast('error');
+    }).then(r => {
+      this.showToast('success');
+    });
+  }
+
+  showToast(type: string) {
+    if (type === 'success') {
+      console.log('Success');
+      this.toast.success('Thank you. We have got your email and we will be in-touch with you for further updates.', {autoClose: true});
+    } else if (type === 'error') {
+      this.toast.error('Uppsss! We have got an error at system. Please try again a little bit later', {autoClose: false});
+    }
+  }
 }
