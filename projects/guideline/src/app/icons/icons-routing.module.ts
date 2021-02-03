3743a5e7b269a7b08dc080490d1c0047f422fd47
Talha Salt <Talha.Salt@logo.com.tr>
2021-02-03 16:24:17 +0300

    feat(guideline): add icons page

---
+++ projects/guideline/src/app/icons/icons-routing.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {IconsComponent} from '@guideline/src/app/icons/icons.component';
+
+const routes: Routes = [{
+  path: '',
+  component: IconsComponent,
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class IconsRoutingModule {
+}
---
+++ projects/guideline/src/app/icons/icons.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {IconsRoutingModule} from './icons-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IconsRoutingModule,
+  ],
+})
+export class IconsModule {
+}
