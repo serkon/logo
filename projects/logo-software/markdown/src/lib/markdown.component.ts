cdba6894dbeffb34af7cf85b2c275caf4c9b3777
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-02-07 15:34:32 +0300

    refactor(markdown): markdown view init and ng change listener

--- projects/logo-software/markdown/src/lib/markdown.component.ts
+++ projects/logo-software/markdown/src/lib/markdown.component.ts
@@ -8,44 +8,62 @@
  * Any reproduction of this material must contain this notice.
  */
 
-import { AfterViewInit, Component, EmbeddedViewRef, Input, OnChanges, SimpleChanges, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnDestroy, SimpleChanges, ViewChild, ViewContainerRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 import * as marked from 'marked';
 import * as hljs from 'highlight.js';
 
 import { DynamicService } from '@logo-software/dynamic';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'logo-markdown',
   styleUrls: ['./markdown.component.scss'],
   templateUrl: './markdown.component.html',
 })
-export class MarkdownComponent implements OnChanges, AfterViewInit {
+export class MarkdownComponent implements OnChanges, AfterViewInit, OnDestroy {
 
   @ViewChild('container', {read: ViewContainerRef}) container!: ViewContainerRef;
   /**
    * @Input() url = '/assets/docs/getstarted.md'
    */
   @Input() url;
-  public string: string = '';
+  @Input() string: string = '';
+  private dataChange = false;
+  private viewInit = false;
+  private viewInit$: Subject<{ change?, ready? }> = new Subject<{ change?, ready? }>();
 
   constructor(private dynamicService: DynamicService, private http: HttpClient, private sanitize: DomSanitizer) {
+    this.viewInit$.subscribe(({change, ready}) => {
+      if (change && ready) {
+        this.container.clear();
+        const componentRef = this.container.createComponent(this.dynamicService.factory(this.string));
+        // const componentNode = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+      }
+    });
   }
 
-  async ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges) {
+    this.dataChange = false;
     this.init();
   }
 
   ngAfterViewInit() {
-    // this.init();
+    this.viewInit = true;
+    this.viewInit$.next({ready: true});
+  }
+
+  ngOnDestroy() {
+    this.viewInit$.unsubscribe();
   }
 
   async init() {
-    this.string = await this.resolve();
-    this.container.clear();
-    const componentRef = this.container.createComponent(this.dynamicService.factory(this.string));
-    // const componentNode = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+    if (this.url) {
+      this.string = await this.resolve();
+    }
+    this.dataChange = true;
+    this.viewInit$.next({change: this.dataChange, ready: this.viewInit});
   }
 
   async resolve() {
