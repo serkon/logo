4d33621e36788db9c504ae9df98599f666dabdbd
Talha SALT <Talha.Salt@logo.com.tr>
2021-02-10 19:48:19 +0300

    chore(guideline): fix import lines tslint

--- projects/playground/src/app/logo/button-sample/button-sample.module.ts
+++ projects/playground/src/app/logo/button-sample/button-sample.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonSampleRoutingModule } from './button-sample-routing.module';
 import { ButtonShowcaseComponent } from './button-showcase/button-showcase.component';
-import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [ButtonShowcaseComponent],
-    imports: [
-        CommonModule,
-        ButtonSampleRoutingModule,
-        FormsModule,
-    ],
+  imports: [CommonModule, ButtonSampleRoutingModule, FormsModule],
   exports: [ButtonShowcaseComponent],
 })
-export class ButtonSampleModule { }
+export class ButtonSampleModule {
+}
