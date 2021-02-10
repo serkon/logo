28c30df08874bdda9d8b0ba03e4e3e5139b0c536
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-02-10 18:22:36 +0300

    refactor(paas-leftbar): improve sample usage of leftbar library

--- projects/paas-leftbar/src/app/app.component.ts
+++ projects/paas-leftbar/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, retry } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 import { LeftbarService } from '@logo-software/leftbar';
+import { Tree, TreeComponent } from '@logo-software/tree';
+import { ModalService } from '@logo-software/modal';
+import { ToastService } from '@logo-software/toast';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +15,102 @@ import { LeftbarService } from '@logo-software/leftbar';
 })
 export class AppComponent implements OnInit {
 
-  constructor(public http: HttpClient, public leftbarService: LeftbarService) {
+  public menuItems: Tree[];
+  public shortcutItems: Tree[];
+  /*public shortcutItems: Tree[] = [
+    {
+      'id': 0,
+      'name': 'Örnek Menü 1',
+      'optional': true,
+      'iconClasses': 'success',
+      'iconPath': '/assets/data/sample-app-icon.svg',
+      'children': [
+        {
+          'id': 1,
+          'name': 'Components',
+          'iconClasses': 'brand',
+          'optional': true,
+          'iconPath': '/assets/data/sample-app-icon.svg',
+          'role': 'ROLE_MENU_CONTROL_TOWER',
+        },
+        {
+          'id': 2,
+          'name': 'Saha Yönetimi',
+          'iconClasses': 'success',
+          'optional': true,
+          'iconPath': '/assets/data/sample-app-icon2.svg',
+          'children': [
+            {
+              'id': 2028,
+              'name': 'Operasyonlar',
+              'iconClasses': 'success',
+              'optional': true,
+              'iconPath': '/assets/data/sample-app-icon3.svg',
+              'role': 'ROLE_MENU_FM_TRANSACTIONS',
+            },
+          ],
+          'classes': 'demo-class',
+          'role': 'ROLE_MENU_FIELD_OP',
+        },
+        {
+          'id': 7,
+          'name': 'Döküman Yönetimi',
+          'iconClasses': 'warning',
+          'optional': true,
+          'iconPath': '/assets/data/sample-app-icon3.svg',
+          'children': [
+            {
+              'id': 2046,
+              'name': 'Raporlar ve Bildirimler',
+              'role': 'ROLE_MENU_DM_DESCRIPTION',
+              'link': '/main/domain/delivery/form',
+              'optional': true,
+              'iconClasses': 'success',
+              'iconPath': '/assets/data/sample-app-icon.svg',
+            },
+            {
+              'id': 2047,
+              'name': 'Tanımlar',
+              'optional': true,
+              'iconClasses': 'warning',
+              'iconPath': '/assets/data/sample-app-icon2.svg',
+              'role': 'ROLE_MENU_DM_TRANSACTIONS',
+            },
+          ],
+          'role': 'ROLE_MENU_DOCUMENT_MGN',
+        },
+      ],
+      'role': 'ROLE_MENU_ROOT',
+    },
+    {
+      'id': 34,
+      'name': 'Örnek Menü 2',
+      'iconClasses': 'danger',
+      'optional': true,
+      'iconPath': '/assets/data/sample-app-icon.svg',
+      'children': [
+        {
+          'id': 1,
+          'name': 'Components',
+          'iconClasses': 'success',
+          'optional': true,
+          'iconPath': '/assets/data/sample-app-icon3.svg',
+          'role': 'TEST USER',
+        },
+      ],
+      'role': 'TEST USER',
+    },
+  ];*/
+
+  @ViewChild(TreeComponent, {read: TreeComponent}) tree: TreeComponent;
+
+  constructor(public http: HttpClient, public leftbarService: LeftbarService, public modal: ModalService, public toast: ToastService) {
   }
 
   ngOnInit(): void {
   }
 
-  public getSampleUserData() {
+  public sampleUserDataGet() {
     this.http.request('GET', '/assets/data/sample-user.json')
       .pipe(
         retry(2),
@@ -27,8 +119,14 @@ export class AppComponent implements OnInit {
       .subscribe(
         data => {
           this.leftbarService.userDataLoad.next(data);
+          this.sampleMenuDataGet();
         },
       );
+  }
+
+
+  public async sampleUserDataGet2() {
+    return await this.http.request('GET', '/assets/data/sample-user.json');
   }
 
   private handleError(error: HttpErrorResponse) {
@@ -39,5 +137,103 @@ export class AppComponent implements OnInit {
     }
     return throwError(
       'Bir hata oluştu. Lütfen daha sonra tekrar deneyiniz.');
+  }
+
+  public sampleAddShortcut() {
+    this.modal.openModal('logo-modal');
+  }
+
+  public sampleSaveAction() {
+    this.modal.closeModal('logo-modal');
+    this.toast.success('Kısayollarınız kayıt edilmiştir.', {autoClose: true});
+  }
+
+  public sampleMenuDataGet() {
+    this.http.request<Tree[]>('GET', '/assets/data/sample-menu.json')
+      .pipe(
+        retry(2),
+        catchError(this.handleError),
+      )
+      .subscribe(
+        data => {
+          this.menuItems = data;
+        },
+      );
+  }
+
+  public sampleShorcutDataGet() {
+    this.http.request<Tree[]>('GET', '/assets/data/sample-shortcut-menu.json')
+      .pipe(
+        retry(2),
+        catchError(this.handleError),
+      )
+      .subscribe(
+        data => {
+          console.log(data);
+          this.shortcutItems = data;
+        },
+      );
+  }
+
+  public sampleDataGet(method: string, url: string) {
+    this.http.request(method, url)
+      .pipe(
+        retry(2),
+        catchError(this.handleError),
+      )
+      .subscribe(
+        data => {
+          return data;
+        },
+      );
+  }
+
+  public sampleItemDataGet(item: Tree) {
+    console.log(item.id);
+    this.http.request<Tree[]>('GET', '/assets/data/sample-menu.json')
+      .pipe(
+        retry(2),
+        catchError(this.handleError),
+      )
+      .subscribe(
+        data => {
+          item['children'] = data;
+          item.isOpen = true;
+        },
+      );
+  }
+
+  public sampleShortcutItemDataGet(item: Tree) {
+    this.http.request<Tree[]>('GET', '/assets/data/sample-shortcut-menu.json')
+      .pipe(
+        retry(2),
+        catchError(this.handleError),
+      )
+      .subscribe(
+        data => {
+          item['children'] = data;
+          item.isOpen = true;
+        },
+      );
+  }
+
+  public sampleHomeAction() {
+    console.log('### Home Clicked');
+  }
+
+  public sampleTenantAction(id) {
+    console.log('### Tanent Action' + id);
+  }
+
+  public sampleAppAction(id) {
+    console.log('### Application Action' + id);
+  }
+
+  public sampleSettingsAction() {
+    console.log('### Settings');
+  }
+
+  public sampleLangAction(id: string) {
+    console.log(id);
   }
 }
--- projects/paas-leftbar/src/app/app.module.ts
+++ projects/paas-leftbar/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { LeftbarModule } from '@logo-software/leftbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TreeModule } from '@logo-software/tree';
+import { ModalModule } from '@logo-software/modal';
+import { ToastModule, ToastService } from '@logo-software/toast';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, LeftbarModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, LeftbarModule, TreeModule, ModalModule, ToastModule],
   bootstrap: [AppComponent],
+  providers: [ToastService],
 })
 export class AppModule {
 }
