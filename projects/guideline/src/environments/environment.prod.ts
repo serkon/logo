77c95d979d1d9cf5cc07a91b55fafe03c9ac850f
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-02-11 08:37:26 +0300

    fix(guideline): fiz mobile usage bugs after import

--- projects/guideline/src/environments/environment.prod.ts
+++ projects/guideline/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAS9btdNCWMqaWOC79OpI0Vy1pZVJYDL5U',
+    authDomain: 'logoelements.firebaseapp.com',
+    databaseURL: 'https://logoelements-default-rtdb.firebaseio.com/',
+    projectId: 'logoelements',
+    storageBucket: 'logoelements.appspot.com',
+    messagingSenderId: '989863561349',
+  },
 };
