4c0455c3e85295745e7c7dc6b79fe59aa6474171
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-02-20 13:58:34 +0300

    feat(cloud): develop general app pages

--- projects/cloud/src/app/app.component.ts
+++ projects/cloud/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, ChangeDetectorRef, Component } from '@angular/core';
+import { HeaderTheme } from '@logo-software/header';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'cloud';
+export class AppComponent implements AfterContentChecked {
+  public headerTheme = HeaderTheme;
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  ngAfterContentChecked(): void {
+    this.cdr.detectChanges();
+  }
+
+  public toggleSearch() {
+    console.log('Search Screen');
+  }
+
+  public toggleHoverMenu(b, e: HTMLElement) {
+    const leftPosition = Math.round(b.target.offsetLeft - 50);
+    e.className.search('opened') <= 0 ? e.className = 'hover-menu opened' : e.className = 'hover-menu';
+    e.className.search('opened') > -1 ? e.style.left = leftPosition + 'px' : e.style.left = '';
+  }
 }
--- projects/cloud/src/app/app.module.ts
+++ projects/cloud/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderModule } from '@logo-software/header';
+import { LinksModule } from '@logo-software/links';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    LinksModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
