d768c6f051e4b253f8f7f679c899816706731b90
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-02-26 06:37:41 +0300

    style(cloud): implement design to main page

--- projects/cloud/src/app/home/home.module.ts
+++ projects/cloud/src/app/home/home.module.ts
@@ -3,19 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { HeroModule } from '@logo-software/hero';
 import { CarouselModule } from '@logo-software/carousel';
+import { CardModule } from '@logo-software/card';
+import { LinksModule } from '@logo-software/links';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CloudFooterComponent } from '../components/cloud-footer/cloud-footer.component';
+import { ProductItemComponent } from '../components/product-item/product-item.component';
+
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CloudFooterComponent, ProductItemComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
     HeroModule,
     CarouselModule,
+    CardModule,
+    LinksModule,
+    LinksModule,
+    LinksModule,
   ],
-  exports: [HomeComponent],
+  exports: [HomeComponent, CloudFooterComponent, ProductItemComponent],
 })
 export class HomeModule {
 }
