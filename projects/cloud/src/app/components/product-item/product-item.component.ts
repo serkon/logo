6b5d929d0ac05be6dee5107c8152fcf05429c225
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-02-26 06:36:59 +0300

    feat(cloud): add product summary item library

---
+++ projects/cloud/src/app/components/product-item/product-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductSummary } from '../../../models/interfaces';
+
+@Component({
+  selector: 'logo-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss'],
+})
+export class ProductItemComponent implements OnInit {
+  @Input() productData: ProductSummary;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log(this.productData);
+  }
+
+}
