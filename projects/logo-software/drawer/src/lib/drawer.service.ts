97196e4cc5feafad6da972ef1e70eec1da12539b
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-02-28 19:13:53 +0300

    docs(drawer): update drawer documentation

--- projects/logo-software/drawer/src/lib/drawer.service.ts
+++ projects/logo-software/drawer/src/lib/drawer.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
+/**
+ * Drawer Service manage drawer options
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class DrawerService {
+  /**
+   * Manage left menu status, default: false. If false menu will be closed.
+   */
   public menu: boolean = false;
+  /**
+   * Title changer
+   */
   public changeTitle$ = new Subject();
+  /**
+   * Set left menu width
+   */
   public setMenuWidth$ = new Subject();
+  /**
+   * Set header text
+   */
   public headerText = '';
+  /**
+   * Hamburger menu status, default is true
+   */
   public isOpenerActive: boolean = true;
+  /**
+   * It is hides header, default is false
+   */
   public hideHeader: boolean = false;
 
   /**
