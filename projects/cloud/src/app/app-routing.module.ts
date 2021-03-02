ac3713c214b7698792f712ada8fe1ed6736405c7
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-03-02 03:05:43 +0300

    refactor(cloud): update tab sample

--- projects/cloud/src/app/app-routing.module.ts
+++ projects/cloud/src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('../../../../projects/playground/src/app/logo/logo.module').then(m => m.LogoModule),
   },
+  {
+    path: 'serkon/:id/patates',
+    loadChildren: () => import('../../../../projects/playground/src/app/logo/button-sample/button-sample.module').then(m => m.ButtonSampleModule),
+  },
 ];
 
 @NgModule({
