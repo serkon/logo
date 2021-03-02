55c48cf31bc413b4e0dc0e8ccdf1aa9637646984
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-03-02 12:00:24 +0300

    refactor(cloud): develop main page

--- projects/cloud/src/app/home/home.component.ts
+++ projects/cloud/src/app/home/home.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { BlogSummary, FAQ, HttpResponse, ProductSummary, SolutionSummary, Testimonial } from '../../models/interfaces';
+import { SolutionService } from '../services/solutions/solution.service';
+import { ProductService } from '../services/product/product.service';
+import { TestimonialsService } from '../services/testimonials/testimonials.service';
+import { FaqService } from '../services/faq/faq.service';
+import { BlogService } from '../services/blog/blog.service';
 
 @Component({
   selector: 'logo-home',
@@ -6,11 +14,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public SolutionSummaries: SolutionSummary;
+  public ProductSummaries: ProductSummary;
+  public Testimonials: Testimonial;
+  public Faqs: FAQ;
+  public BlogData: HttpResponse<BlogSummary>;
+  public BlogSummaries: BlogSummary;
+  private SolutionSummaryData: Observable<HttpResponse<SolutionSummary>>;
+  private ProductSummaryData: HttpResponse<ProductSummary>;
+  private TestimonialsData: HttpResponse<Testimonial>;
+  private FaqData: HttpResponse<FAQ>;
 
-  constructor() {
+  constructor(
+    private solutionService: SolutionService,
+    private productService: ProductService,
+    private testimonialsService: TestimonialsService,
+    private faqService: FaqService,
+    private blogService: BlogService,
+  ) {
   }
 
   ngOnInit(): void {
+    this.loadSolutionSummaries();
+    this.loadProductSummaries();
+    this.loadTestimonials();
+    this.loadFaqs();
+    this.loadBlogSummaries();
+  }
+
+  private async loadSolutionSummaries() {
+    this.SolutionSummaryData = this.solutionService.getSolutionSummaries();
+    this.SolutionSummaryData.subscribe(res => {
+      this.SolutionSummaries = res.data;
+    });
+  }
+
+  private async loadProductSummaries() {
+    this.ProductSummaryData = await this.productService.getProductSummaries();
+    this.ProductSummaries = this.ProductSummaryData.data;
+  }
+
+  private async loadTestimonials() {
+    this.TestimonialsData = await this.testimonialsService.getTestimonials();
+    this.Testimonials = this.TestimonialsData.data;
+  }
+
+  private async loadFaqs() {
+    this.FaqData = await this.faqService.getFaqs();
+    this.Faqs = this.FaqData.data;
+  }
+
+  private async loadBlogSummaries() {
+    this.BlogData = await this.blogService.getBlogSummary();
+    this.BlogSummaries = this.BlogData.data;
   }
 
 }
--- projects/cloud/src/app/home/home.module.ts
+++ projects/cloud/src/app/home/home.module.ts
@@ -5,11 +5,13 @@ import { HeroModule } from '@logo-software/hero';
 import { CarouselModule } from '@logo-software/carousel';
 import { CardModule } from '@logo-software/card';
 import { LinksModule } from '@logo-software/links';
+import { AccordionModule } from '@logo-software/accordion';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { CloudFooterComponent } from '../components/cloud-footer/cloud-footer.component';
 import { ProductItemComponent } from '../components/product-item/product-item.component';
+import { PagingModule } from '@logo-software/paging';
 
 
 @NgModule({
@@ -23,6 +25,9 @@ import { ProductItemComponent } from '../components/product-item/product-item.co
     LinksModule,
     LinksModule,
     LinksModule,
+    CarouselModule,
+    AccordionModule,
+    PagingModule,
   ],
   exports: [HomeComponent, CloudFooterComponent, ProductItemComponent],
 })
