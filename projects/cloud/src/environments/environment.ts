55b3b19067700603bcb4fb33e27aaaed1d3edc04
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-03-03 14:36:04 +0300

    style(cloud): import code syntax fix

--- projects/cloud/src/environments/environment.ts
+++ projects/cloud/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { AuthorizationType } from '../../../logo-software/idm/src/lib/interface/authorization-type';
+import { AuthorizationType } from '@logo-software/idm';
 
 export const environment = {
   production: false,
