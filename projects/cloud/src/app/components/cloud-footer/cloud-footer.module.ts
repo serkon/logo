6747b82e108b80304952864cac00fee874fecab5
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-03-04 07:02:23 +0300

    refactor(cloud): add module for footer to import

---
+++ projects/cloud/src/app/components/cloud-footer/cloud-footer.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LinksModule } from '@logo-software/links';
+
+import { CloudFooterComponent } from '@cloud/app/components/cloud-footer/cloud-footer.component';
+
+@NgModule({
+  declarations: [CloudFooterComponent],
+  imports: [
+    CommonModule,
+    LinksModule,
+  ],
+  exports: [CloudFooterComponent],
+})
+export class CloudFooterModule {
+}
