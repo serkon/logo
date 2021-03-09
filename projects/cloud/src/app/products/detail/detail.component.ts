97345f4ea9b64788604594dc4b9c6e84e3a29945
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-03-09 11:48:18 +0300

    feat(cloud): add detail page

---
+++ projects/cloud/src/app/products/detail/detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'logo-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss'],
+})
+export class DetailComponent implements OnInit {
+  public productId: string = '';
+
+  constructor(private route: ActivatedRoute) {
+    this.productId = this.route.snapshot.params.slug;
+  }
+
+  ngOnInit(): void {
+  }
+}
