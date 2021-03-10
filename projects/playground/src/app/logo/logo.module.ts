9645dc2bdc3cd0ee009d712951d3e7e9df0d1798
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-03-10 07:43:41 +0300

    refactor(playground): update for component samples

--- projects/playground/src/app/logo/logo.module.ts
+++ projects/playground/src/app/logo/logo.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { LogoRoutingModule } from './logo-routing.module';
 import { LogoComponent } from './logo.component';
+import { AccordionSampleModule } from './accordion-sample/accordion-sample.module';
+import { DatepickerSampleModule } from './datepicker-sample/datepicker-sample.module';
+import { InputSampleModule } from './input-sample/input-sample.module';
+import { PagingSampleModule } from './paging-sample/paging-sample.module';
+import { SelectSampleModule } from './select-sample/select-sample.module';
 
 @NgModule({
   declarations: [LogoComponent],
-  imports: [CommonModule, LogoRoutingModule],
+  imports: [
+    CommonModule,
+    LogoRoutingModule,
+    AccordionSampleModule,
+    DatepickerSampleModule,
+    InputSampleModule,
+    PagingSampleModule,
+    SelectSampleModule,
+  ],
   exports: [],
 })
 export class LogoModule {
