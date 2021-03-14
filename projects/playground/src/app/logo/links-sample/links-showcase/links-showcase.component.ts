f6b92c5c4f691211aac144aa67711ab265d2b21d
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-03-14 20:33:25 +0300

    refactor(playground): fix links sample component import path

--- projects/playground/src/app/logo/links-sample/links-showcase/links-showcase.component.ts
+++ projects/playground/src/app/logo/links-sample/links-showcase/links-showcase.component.ts
@@ -3,8 +3,8 @@ import { Link } from '@logo-software/links';
 
 @Component({
   selector: 'logo-link-showcase',
-  templateUrl: './link-showcase.component.html',
-  styleUrls: ['./link-showcase.component.scss'],
+  templateUrl: './links-showcase.component.html',
+  styleUrls: ['./links-showcase.component.scss'],
 })
 export class LinksShowcaseComponent {
 
