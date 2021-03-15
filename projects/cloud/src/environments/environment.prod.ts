da0cfc1fb88a9349d4fea3141cfa5a7fd9f1dbf8
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-03-15 13:55:46 +0300

    build(cloud): change enviroment

--- projects/cloud/src/environments/environment.prod.ts
+++ projects/cloud/src/environments/environment.prod.ts
@@ -1,3 +1,69 @@
+import { AuthorizationType } from '@logo-software/idm';
+
 export const environment = {
   production: true,
+  IDM: {
+    CLIENT_ID: 'b7d27891-7efa-4133-91f0-38ff5b22ac7f',
+    CONFIG: {
+      URI: 'http://dev-linux.logo-paas.com:5100',
+      TENANT: {
+        DEFAULT: '/api/users/{userId}/defaulttenant',
+      },
+      TOKEN: {
+        AUTH_TYPE: AuthorizationType.IMPLICIT,
+        GET: '/api/Token/GetToken',
+        VALIDATE: '/api/Token/ValidateToken',
+      },
+      DIRECTION: {
+        REDIRECT: true,
+        '403': '/forbidden',
+        '500': '/server-error',
+      },
+      RETURN_URI: 'http://localhost:4700',
+      LOGIN_PAGE: {
+        URI: '/login',
+        STATUS: false,
+      },
+    },
+  },
+  IDM_OLD: {
+    CLIENT: '2519ef34-66a0-4e5b-a333-089eb34edfc5',
+    URL: 'http://dev-linux.logo-paas.com:5100',
+    TENANT: '/api/users/{userId}/defaulttenant',
+    TOKEN: {
+      GET: '/api/Token/GetToken',
+      VALIDATE: '/api/Token/ValidateToken',
+    },
+  },
+  api: {
+    baseURL: 'http://mocks.paas.test/api',
+    solution: {
+      prefix: 'solution',
+      summary: 'summary/list',
+      detail: 'detail',
+    },
+    product: {
+      prefix: 'product',
+      summary: 'summary/list',
+      detail: 'detail',
+      segment: 'segment/list',
+      sector: 'sector/list',
+      reference: 'reference/list',
+      sticker: 'sticker/detail',
+      seller: 'seller/detail',
+      finder: {
+        step: 'step/list',
+        questions: 'questions/list',
+        expectations: 'expectation/list',
+      },
+    },
+    blog: {
+      prefix: 'blog',
+      summary: 'summary/list',
+      detail: 'detail',
+    },
+    tag: 'tag/list',
+    faq: 'faq/list',
+    testimonial: 'testimonial/list',
+  },
 };
