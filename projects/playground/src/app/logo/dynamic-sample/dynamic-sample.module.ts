afca13c1d07094ef4dbcb7b89740a947cbb5bf69
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-03-17 17:06:19 +0300

    refactor(playground): add dynamic sample showcase component

--- projects/playground/src/app/logo/dynamic-sample/dynamic-sample.module.ts
+++ projects/playground/src/app/logo/dynamic-sample/dynamic-sample.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DynamicModule } from '@logo-software/dynamic';
+
 import { DynamicSampleRoutingModule } from './dynamic-sample-routing.module';
 import { DynamicShowcaseComponent } from './dynamic-showcase/dynamic-showcase.component';
 
 @NgModule({
   declarations: [DynamicShowcaseComponent],
-  imports: [
-    CommonModule,
-    DynamicSampleRoutingModule,
-  ],
+  imports: [CommonModule, DynamicSampleRoutingModule, DynamicModule],
   exports: [DynamicShowcaseComponent],
 })
-export class DynamicSampleModule { }
+export class DynamicSampleModule {
+}
--- projects/playground/src/app/logo/dynamic-sample/dynamic-showcase/dynamic-showcase.component.ts
+++ projects/playground/src/app/logo/dynamic-sample/dynamic-showcase/dynamic-showcase.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { PlaygroundModule } from '@logo-software/playground';
+import { BreadcrumbModule } from '@logo-software/breadcrumb';
 
 @Component({
   selector: 'logo-dynamic-showcase',
   templateUrl: './dynamic-showcase.component.html',
   styleUrls: ['./dynamic-showcase.component.scss'],
 })
-export class DynamicShowcaseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DynamicShowcaseComponent {
+  imports = [PlaygroundModule, BreadcrumbModule];
+  text = `
+    <logo-playground path="#/logo/button-sample/button-showcase/button-showcase.component" context='{"title": "Button Demo", "button": true}' ></logo-playground>
+    <logo-breadcrumb [breadcrumb]="[{name:'Home', link: '/'}, {name:'Products', link: '/products'}, {name: 'Potato'}]" [isLight]="false" [size]="'medium'"></logo-breadcrumb>
+    `;
 }
