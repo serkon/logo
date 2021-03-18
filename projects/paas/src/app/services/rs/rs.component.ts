5683d7d83a71058e8bb3d1fccde6cdf9a48f9aa0
Talha SALT <Talha.Salt@logo.com.tr>
2021-03-18 07:41:35 +0300

    fix(paas): rs component done

--- projects/paas/src/app/services/rs/rs.component.ts
+++ projects/paas/src/app/services/rs/rs.component.ts
@@ -11,86 +11,29 @@ export class RsComponent implements AfterViewInit {
 
   public sliderItems: SliderItem[];
   public opinions = [1];
-  public boxes = [
-    {
-      icon: '/assets/images/services/cc/section-02-icon-01.svg',
-      description: 'Uygulama tanımlarınızı, ürünleştirerek API uçları ya da arayüzleri kullanarak Bulut Yönetim Servisine tanımlayın.',
-    },
-    {
-      icon: '/assets/images/services/cc/section-02-icon-02.svg',
-      description: 'Hesaplarınızı API uçları ya da arayüzler yardımıyla kolayca tanımlayın.',
-    },
-    {
-      icon: '/assets/images/services/cc/section-02-icon-03.svg',
-      description: 'Hesap kullanıcılarızı ve uygulama yetkilerini arayüzler ya da API uçları ile yönetin.',
-    },
-    {
-      icon: '/assets/images/services/cc/section-02-icon-04.svg',
-      description: 'Hesabınız oluşurken durumunu Kafka mesajları ile takip edin.',
-    },
-    {
-      icon: '/assets/images/services/cc/section-02-icon-05.svg',
-      description: 'Hesaba kullanıcı tanımlama işlemlerini E-Posta aktivasyonları ile kullanıcı onayından geçirin.',
-    },
-    {
-      icon: '/assets/images/services/cc/section-02-icon-06.svg',
-      description: 'Sunucu tanımlarınızı yapın ve uygulamanızın hangi sunucuda çalışacağını bulut yönetim servisine bırakın.',
-    },
-  ];
+
   private bodyHeight: number;
 
   constructor(@Inject(DOCUMENT) private document) {
     this.sliderItems = [
       {
-        icon: '/assets/images/services/idm/section-04-01.svg',
-        abbr: 'Uygulamalarınızın menüleri için merkezi yönetim',
+        icon: '/assets/images/services/rs/rs-specs-icon-1.svg',
+        abbr: 'Cok kiracılı altyapı (Multitenancy)',
         title: '',
-        description: 'Hesabınıza tanımlı uygulamaların menülerini bir yerden yönetin',
+        description: 'Rapor şablonları ve çıktıları kiracı bazında farklı veritabanlarında tutulmaktadır.',
         url: '',
         classes: '',
       },
       {
-        icon: '/assets/images/services/idm/section-04-02.svg',
-        abbr: 'Çoklu Hesap (Multitenant) İşlemleri',
+        icon: '/assets/images/services/rs/rs-specs-icon-2.svg',
+        abbr: 'Java uygulamaları için client desteği',
         title: '',
-        description: 'Kullanıcı şifrelerinin güvenli bir şekilde (MD5, SHA1, SHA256, SHA384, SHA512 şifreleme yöntemleri ile ) tutulduğundan emin olun',
+        description: 'Java uygulamaları, java client ‘ını kullanarak Raporlama Servisine kolayca entegre olabilirler.',
         url: '',
         classes: '',
       },
       {
-        icon: '/assets/images/services/idm/section-04-03.svg',
-        abbr: 'Hızlı Kısayol İşlemleri',
-        title: '',
-        description: 'Verilen yetkiler doğrultusunda kısayollarınızı görüntüleyip, sürükle, bırak ie işlemlerinizi hızlıca tamamlayın',
-        url: '',
-        classes: '',
-      },
-      {
-        icon: '/assets/images/services/idm/section-04-04.svg',
-        abbr: 'Masaüstünüzü Tasarlayın',
-        title: '',
-        description: 'Tamamen kişisel masaüstünüzü oluşturup hemen kullanmaya başlayın',
-        url: '',
-        classes: '',
-      },
-      {
-        icon: '/assets/images/services/idm/section-04-05.svg',
-        abbr: 'Aradığınızı Bulun',
-        title: '',
-        description: 'Her ekranda sizi karşılayan arama çubukları yardımıyla menü ve kısayollarınıza anında hızlı erişim sağlayın',
-        url: '',
-        classes: '',
-      },
-      {
-        icon: '/assets/images/services/idm/section-04-06.svg',
-        abbr: 'Uygulamalar içerisinde arama',
-        title: '',
-        description: 'Menü serviste yapılan aramalar, kayıtlı uygulamalar içerisinde de gerçekleşsin',
-        url: '',
-        classes: '',
-      },
-      {
-        icon: '/assets/images/services/idm/section-04-07.svg',
+        icon: '/assets/images/services/rs/rs-specs-icon-3.svg',
         abbr: 'Swagger UI Destekli API Uçları  ',
         title: '',
         description: 'Desteklenen API uçlarına Swagger UI üzerinden kolayca ulaşın',
@@ -98,10 +41,10 @@ export class RsComponent implements AfterViewInit {
         classes: '',
       },
       {
-        icon: '/assets/images/services/idm/section-04-08.svg',
-        abbr: 'Çoklu Dil Desteği',
+        icon: '/assets/images/services/rs/rs-specs-icon-4.svg',
+        abbr: 'Ölçeklenebilir yapı',
         title: '',
-        description: 'Menü servis ile tek bir adımda sayfalarınızın dilini değiştirin',
+        description: 'Oturum bağımsız mimarisi ile Raporlama servisini çoklayarak kullanabilirsiniz.',
         url: '',
         classes: '',
       },
