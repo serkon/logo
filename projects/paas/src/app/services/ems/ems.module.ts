6c78085ed0e6ff68e838b69b01cf6755145db140
Talha SALT <Talha.Salt@logo.com.tr>
2021-03-24 23:46:12 +0300

    refactor(paas): refactor ems component

--- projects/paas/src/app/services/ems/ems.module.ts
+++ projects/paas/src/app/services/ems/ems.module.ts
@@ -3,25 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { CarouselModule } from '@logo-software/carousel';
 import { LinksModule } from '@logo-software/links';
+
+import { CoreServicesModule } from '@paas/src/components/core-services/core-services.module';
+import { UserReviewModule } from '@paas/src/components/user-review/user-review.module';
 import { FooterModule } from '@paas/src/components/footer/footer.module';
 import { HeaderModule } from '@paas/src/components/header/header.module';
+
 import { EmsRoutingModule } from './ems-routing.module';
 import { EmsComponent } from './ems.component';
-import {CoreServicesModule} from '@paas/src/components/core-services/core-services.module';
-import {UserReviewModule} from '@paas/src/components/user-review/user-review.module';
 
 @NgModule({
   declarations: [EmsComponent],
-    imports: [
-        CommonModule,
-        EmsRoutingModule,
-        HeaderModule,
-        FooterModule,
-        CarouselModule,
-        LinksModule,
-        CoreServicesModule,
-        UserReviewModule,
-    ],
+  imports: [
+    CommonModule,
+    EmsRoutingModule,
+    HeaderModule,
+    FooterModule,
+    CarouselModule,
+    LinksModule,
+    CoreServicesModule,
+    UserReviewModule,
+  ],
   exports: [EmsComponent],
 })
 export class EmsModule {
