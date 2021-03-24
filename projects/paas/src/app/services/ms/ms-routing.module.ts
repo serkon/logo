084ddd89616bf90024d1e4d51e5ab4f4a3d9c388
Talha SALT <Talha.Salt@logo.com.tr>
2021-03-24 23:44:51 +0300

    refactor(paas): refactor ms component

--- projects/paas/src/app/services/ms/ms-routing.module.ts
+++ projects/paas/src/app/services/ms/ms-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { MsComponent } from '@paas/src/app/services/ms/ms.component';
 
 const routes: Routes = [
--- projects/paas/src/app/services/ms/ms.module.ts
+++ projects/paas/src/app/services/ms/ms.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CarouselModule } from '@logo-software/carousel';
 import { LinksModule } from '@logo-software/links';
+
 import { FooterModule } from '@paas/src/components/footer/footer.module';
 import { HeaderModule } from '@paas/src/components/header/header.module';
+
 import { MsRoutingModule } from './ms-routing.module';
 import { MsComponent } from './ms.component';
 
