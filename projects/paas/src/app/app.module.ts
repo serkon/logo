97fc8dbff91fc5335198bb9808b20dcaa8423efa
Talha SALT <Talha.Salt@logo.com.tr>
2021-03-24 10:09:37 +0300

    feat(paas): fix header

--- projects/paas/src/app/app.module.ts
+++ projects/paas/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CursorModule } from '@logo-software/cursor';
+import {HeaderModule} from '@logo-software/header';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LinksModule } from '@logo-software/links';
