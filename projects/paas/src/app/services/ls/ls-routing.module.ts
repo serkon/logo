5b570b920b20ad98ad894991cfb35ec81b326e00
Talha SALT <Talha.Salt@logo.com.tr>
2021-03-24 23:45:25 +0300

    refactor(paas): refactor ls component

--- projects/paas/src/app/services/ls/ls-routing.module.ts
+++ projects/paas/src/app/services/ls/ls-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LsComponent } from '@paas/src/app/services/ls/ls.component';
 
 const routes: Routes = [
--- projects/paas/src/app/services/ls/ls.module.ts
+++ projects/paas/src/app/services/ls/ls.module.ts
@@ -3,25 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { CarouselModule } from '@logo-software/carousel';
 import { LinksModule } from '@logo-software/links';
+
 import { FooterModule } from '@paas/src/components/footer/footer.module';
 import { HeaderModule } from '@paas/src/components/header/header.module';
+import { CoreServicesModule } from '@paas/src/components/core-services/core-services.module';
+import { UserReviewModule } from '@paas/src/components/user-review/user-review.module';
+
 import { LsRoutingModule } from './ls-routing.module';
 import { LsComponent } from './ls.component';
-import {CoreServicesModule} from '@paas/src/components/core-services/core-services.module';
-import {UserReviewModule} from '@paas/src/components/user-review/user-review.module';
 
 @NgModule({
   declarations: [LsComponent],
-    imports: [
-        CommonModule,
-        LsRoutingModule,
-        HeaderModule,
-        FooterModule,
-        CarouselModule,
-        LinksModule,
-        CoreServicesModule,
-        UserReviewModule,
-    ],
+  imports: [
+    CommonModule,
+    LsRoutingModule,
+    HeaderModule,
+    FooterModule,
+    CarouselModule,
+    LinksModule,
+    CoreServicesModule,
+    UserReviewModule,
+  ],
   exports: [LsComponent],
 })
 export class LsModule {
