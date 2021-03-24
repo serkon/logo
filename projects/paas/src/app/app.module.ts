56aed1eb1e7103645160ca7a520371c71e3bf82e
Talha SALT <Talha.Salt@logo.com.tr>
2021-03-24 23:49:04 +0300

    refactor(paas): refactor main component

--- projects/paas/src/app/app.module.ts
+++ projects/paas/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CursorModule } from '@logo-software/cursor';
-import {HeaderModule} from '@logo-software/header';
+import { HeaderModule } from '@logo-software/header';
+import { LinksModule } from '@logo-software/links';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LinksModule } from '@logo-software/links';
 
 @NgModule({
   declarations: [AppComponent],
-
   imports: [BrowserModule, AppRoutingModule, CursorModule, HeaderModule, LinksModule],
   providers: [],
   bootstrap: [AppComponent],
