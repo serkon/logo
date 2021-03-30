485bd0bb9c95998f603dc725c541c4a6b559a032
Talha SALT <Talha.Salt@logo.com.tr>
2021-03-30 17:51:34 +0300

    feat(switch): add theme support to switch component

--- projects/logo-software/switch/src/lib/switch.component.ts
+++ projects/logo-software/switch/src/lib/switch.component.ts
@@ -1,4 +1,29 @@
-import { Component, Input } from '@angular/core';
+/**
+ * @license
+ * Copyright LOGO YAZILIM SANAYİ VE TİCARET A.Ş. All Rights Reserved.
+ *
+ * Save to the extent permitted by law, you may not use, copy, modify,
+ * distribute or create derivative works of this material or any part
+ * of it without the prior written consent of LOGO YAZILIM SANAYİ VE TİCARET A.Ş. Limited.
+ * Any reproduction of this material must contain this notice.
+ */
+
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+/**
+ * Here is the switch component usage.
+ * Add the below code to your code stack and give initializer parameters.
+ *
+ * <sub>app.component.html</sub>
+ *
+ * ```html
+ * <logo-switch
+ * class="large success"
+ * [disabled]="false"
+ * [rounded]="true"
+ * ></logo-switch>
+ * ```
+ */
 
 @Component({
   selector: 'logo-switch',
@@ -6,13 +31,25 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./switch.component.scss'],
 })
 export class SwitchComponent {
-  @Input() classes: string;
-  @Input() disabled: boolean = false;
-  @Input() rounded: boolean = true;
-  toggleState: boolean = false;
+  /*
+    Switch Toggle's defines colors via Logo-Elements theme colors.
+   */
+  @Input() theme: 'primary' | 'light' | 'success' | 'warning' | 'danger' = 'primary';
+  /*
+    Switch Toggle's defines disabling status.
+   */
+  @Input() isDisabled: boolean = false;
+  /*
+    Switch Toggle's defiens shape status.
+   */
+  @Input() isRounded: boolean = true;
+  /*
+    Switch Toggle's defiens default on/off states.
+   */
+  @Input() toggleState: boolean = false;
 
-  public toggle() {
-    if (!this.disabled) {
+  private toggleSwitch() {
+    if (!this.isDisabled) {
       this.toggleState = !this.toggleState;
     }
   }
--- projects/logo-software/switch/src/lib/switch.module.ts
+++ projects/logo-software/switch/src/lib/switch.module.ts
@@ -1,7 +1,42 @@
+/**
+ * @license
+ * Copyright LOGO YAZILIM SANAYİ VE TİCARET A.Ş. All Rights Reserved.
+ *
+ * Save to the extent permitted by law, you may not use, copy, modify,
+ * distribute or create derivative works of this material or any part
+ * of it without the prior written consent of LOGO YAZILIM SANAYİ VE TİCARET A.Ş. Limited.
+ * Any reproduction of this material must contain this notice.
+ */
 import { NgModule } from '@angular/core';
-import { SwitchComponent } from './switch.component';
 import { CommonModule } from '@angular/common';
 
+import { SwitchComponent } from './switch.component';
+
+/**
+ * Switch toggle provides display checked/unchecked (On/Off) state on the button.
+ *
+ * ### Installation
+ *
+ * All public npm packages of Logo Software is at [https://www.npmjs.com/~logofe](https://www.npmjs.com/~logofe). To
+ * install Switch Module:
+ *
+ * ```bash
+ * $ npm set registry https://registry.npmjs.org/
+ * $ npm install @logo-software/switch -s
+ * ```
+ *
+ * Just import it to your project of `@NgModule` import section.
+ *
+ * ```typescript
+ * import { SwitchModule } from '@logo-software/switch';
+ *
+ * @NgModule({
+ *  imports: [CommonModule, SwitchModule],
+ * })
+ * export class AppModule {
+ * }
+ * ```
+ */
 @NgModule({
   declarations: [SwitchComponent],
   imports: [
