0c7c094ddd357ca88ea002a1eae7c2eb2ce75faf
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-03-30 18:34:45 +0300

    refactor(showcase): update badge sample pages and update codelint

--- projects/playground/src/app/logo/badge-sample/badge-sample.module.ts
+++ projects/playground/src/app/logo/badge-sample/badge-sample.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BadgeModule } from '@logo-software/badge';
+
 import { BadgeSampleRoutingModule } from './badge-sample-routing.module';
 import { BadgeShowcaseComponent } from './badge-showcase/badge-showcase.component';
-import { BadgeModule } from '@logo-software/badge';
 
 @NgModule({
   declarations: [BadgeShowcaseComponent],
