623f56fdc1a295fe074d5181a48a81e44f4de9e2
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-04-12 14:07:53 +0300

    refactor(showcase): code refactor of datepicker sample

--- projects/playground/src/app/logo/datepicker-sample/datepicker-sample.module.ts
+++ projects/playground/src/app/logo/datepicker-sample/datepicker-sample.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DatepickerModule } from '@logo-software/datepicker';
+
 import { DatepickerSampleRoutingModule } from './datepicker-sample-routing.module';
 import { DatepickerShowcaseComponent } from './datepicker-showcase/datepicker-showcase.component';
-import { DatepickerModule } from '@logo-software/datepicker';
 
 @NgModule({
   declarations: [DatepickerShowcaseComponent],
--- projects/playground/src/app/logo/datepicker-sample/datepicker-showcase/datepicker-showcase.component.ts
+++ projects/playground/src/app/logo/datepicker-sample/datepicker-showcase/datepicker-showcase.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'logo-datepicker-showcase',
   templateUrl: './datepicker-showcase.component.html',
   styleUrls: ['./datepicker-showcase.component.scss'],
 })
-export class DatepickerShowcaseComponent implements OnInit {
-
+export class DatepickerShowcaseComponent {
   public datepicker = {
     value: '05.04.2021',
     min: '01.01.1900',
     max: '31.12.2999',
     second: '31.06.2021',
   };
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-
-  }
-
 }
