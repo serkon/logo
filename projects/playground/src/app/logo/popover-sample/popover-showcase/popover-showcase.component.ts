1c6f8fa272b3c4053d59d350b82048d63c93061c
Serhan GÜRBÜZ <serhan.gurbuz@logo.com.tr>
2021-05-03 16:13:44 +0300

    refactor(playground): update popover sample with inner datepicker

--- projects/playground/src/app/logo/popover-sample/popover-showcase/popover-showcase.component.ts
+++ projects/playground/src/app/logo/popover-sample/popover-showcase/popover-showcase.component.ts
@@ -14,6 +14,23 @@ export class PopoverShowcaseComponent {
   public PopoverPosition = PopoverPosition;
 
   public datepicker = {
-    value: '05.04.2021',
+    value: null,
+    min: new Date('05.16.2021'),
+    max: '31.12.2999',
+    second: '31.06.2021',
   };
+
+  public test = 111;
+
+  dateChanged(value) {
+    console.log('Date changed: ', value);
+  }
+
+  testMethod($event) {
+    this.test = Number($event.target.value) < 120 ? 130 : Number($event.target.value);
+  }
+
+  popOverClosed($event) {
+    console.log($event);
+  }
 }
