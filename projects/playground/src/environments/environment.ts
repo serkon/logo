8461e9f3c52ba14913fc2e2886ead3959d5ac215
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-05-04 22:20:04 +0300

    refactor(datepicker): add idm configuration to env

--- projects/playground/src/environments/environment.ts
+++ projects/playground/src/environments/environment.ts
@@ -2,8 +2,34 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { AuthorizationType } from '@logo-software/idm';
+
 export const environment = {
   production: false,
+  IDM: {
+    CLIENT_ID: 'b7d27891-7efa-4133-91f0-38ff5b22ac7f',
+    CONFIG: {
+      URI: 'http://dev-linux.logo-paas.com:5100',
+      TENANT: {
+        DEFAULT: '/api/users/{userId}/defaulttenant',
+      },
+      TOKEN: {
+        AUTH_TYPE: AuthorizationType.IMPLICIT,
+        GET: '/api/Token/GetToken',
+        VALIDATE: '/api/Token/ValidateToken',
+      },
+      DIRECTION: {
+        REDIRECT: true,
+        '403': '/forbidden',
+        '500': '/server-error',
+      },
+      RETURN_URI: 'http://design.logo.com.tr',
+      LOGIN_PAGE: {
+        URI: '/login',
+        STATUS: false,
+      },
+    },
+  },
 };
 
 /*
